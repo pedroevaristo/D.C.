6f1cfd70c19ab2ca975a89793a72f942f8b1cf6a
pedroeva <pedrodzprogramacao@gmail.com>
2024-02-16 19:16:35 -0300

    16/02/2024 19:16

--- download.py
+++ download.py
@@ -1,10 +1,38 @@
 from pytube import YouTube as yt
-print ("Downloading")
+
+opcoes = """
+ itag=22 type=video/mp4 res=720p fps=30fps
+ itag=43 type=video/webm res=360p fps=30fps 
+ itag=18 type=video/mp4 res=360p fps=30fps  
+ itag=36 type=video/3gpp res=240p fps=30fps  
+ itag=17 type=video/3gpp res=144p fps=30fps 
+ itag=137 type=video/mp4 res=1080p fps=30fps 
+ itag=136 type=video/mp4 res=720p fps=30fps 
+ itag=135 type=video/mp4 res=480p fps=30fps
+ itag=134 type=video/mp4 res=360p fps=30fps 
+ itag=133 type=video/mp4 res=240p fps=30fps 
+ itag=160 type=video/mp4 res=144p fps=30fps 
+ itag=140 type=audio/mp4 abr=128kbps 
+ itag=171 type=audio/webm abr=128kbps
+
+"""
+
 def download_link(pego_link):
-    
-    resolucao = input("Qual resolução quer usar? ")
-    try:
-        highest_resolution_stream = pego_link.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
-        print(highest_resolution_stream.itag)
-    except Exception as e:
-        print("erro ao baixar o vídeo")
+    resolucao = input("Qual resolução quer usar? Entre as opções{opcoes}")
+    for linha in opcoes.splitlines():
+        if linha.startswith(f"itag={resolucao}"):
+            try:
+                
+            
+                print("Downloading")
+            except Exception as e:
+                print("erro ao baixar o vídeo")
+
+                (
+                    """toda vez que digito algo mas parece queue é por padrão 1
+                        Qual resolução quer usar? 137
+                        22"""
+                )
+        else:
+            print("não apareceu a opção")
+        
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pytube
 import convert
 from download import download_link
-#https://www.youtube.com/watch?v=SrnWp5O0DEs&pp=ygUMbm8gbW9yZSBqYWNr
+
+# https://www.youtube.com/watch?v=SrnWp5O0DEs&pp=ygUMbm8gbW9yZSBqYWNr
+
 
 def consulta(link):
     pego_link = pytube.YouTube(link)
 
     if link and "youtube.com" in link:
-        
-        opcao = input(f"\n-----------------------\nTitulo:  {pego_link.title} Duração :  {round(pego_link.length / 60)}\n\n---------escolha entre as opções--------\n 1- Baixar vídeo \n 2- Converter vídeo para música \n--------------\n")
-            
+        opcao = input(
+            f"\n-----------------------\nTitulo:  {pego_link.title} Duração :  {round(pego_link.length / 60)} minutos\n\n---------escolha entre as opções--------\n 1- Baixar vídeo \n 2- Converter vídeo para música \n--------------\n"
+        )
+
         match opcao:
-            case 1:
+            case "1":
                 download_link(pego_link)
-            #case 2:
+            # case 2:
 
     else:
         print("Esse link não direciona ao youtube")
+
 
 if __name__ == "__main__":
     link = input("Copie e cole a URL da música aqui:")
